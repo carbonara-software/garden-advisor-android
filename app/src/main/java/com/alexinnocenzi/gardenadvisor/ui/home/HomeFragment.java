fdb41d65cf9a61c756703f23ed68cc1d791d8944
AlexInn <alex.innocenzi.work@gmail.com>
2024-05-24 14:09:25 +0200

    Added Navigation and ViewBinding Added first Fragment

---
+++ app/src/main/java/com/alexinnocenzi/gardenadvisor/ui/home/HomeFragment.java
@@ -0,0 +1,32 @@
+package com.alexinnocenzi.gardenadvisor.ui.home;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.alexinnocenzi.gardenadvisor.R;
+import com.alexinnocenzi.gardenadvisor.databinding.FragmentHomeBinding;
+
+public class HomeFragment extends Fragment {
+    FragmentHomeBinding binding;
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        binding = FragmentHomeBinding.inflate(inflater,container,false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+}
