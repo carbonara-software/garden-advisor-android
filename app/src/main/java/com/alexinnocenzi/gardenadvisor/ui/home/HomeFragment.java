02fa70f9d89b0b28d61ce1762ece5c3f0302fc4e
AlexInn <alex.innocenzi.work@gmail.com>
2024-05-24 18:53:26 +0200

    Created Loading dialog with no background it can still be closed by clicking outside. Added Lottie lib

--- app/src/main/java/com/alexinnocenzi/gardenadvisor/ui/home/HomeFragment.java
+++ app/src/main/java/com/alexinnocenzi/gardenadvisor/ui/home/HomeFragment.java
@@ -1,15 +1,20 @@
 package com.alexinnocenzi.gardenadvisor.ui.home;
 
+import static com.alexinnocenzi.gardenadvisor.util.LogUtil.loge;
+
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.alexinnocenzi.gardenadvisor.R;
 import com.alexinnocenzi.gardenadvisor.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
@@ -24,6 +29,14 @@ public class HomeFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        loge("Setto");
+        binding.testo.setOnClickListener(this::nuovoClick);
+    }
+
+    private void nuovoClick(View view) {
+        loge("Click");
+        NavController navController = Navigation.findNavController(getActivity(), R.id.nav_host_fragment);
+        navController.navigate(R.id.loading_dialog);
     }
 
 }
---
+++ app/src/main/java/com/alexinnocenzi/gardenadvisor/util/LogUtil.java
@@ -0,0 +1,27 @@
+package com.alexinnocenzi.gardenadvisor.util;
+
+import android.util.Log;
+
+public class LogUtil {
+
+    private static final String TAG = "GARDENADVISOR";
+    public static void logi(String message){
+        Log.i(TAG,message);
+    }
+    public static void logd(String message){
+        Log.d(TAG,message);
+    }
+    public static void logw(String message){
+        Log.w(TAG,message);
+    }
+
+    public static void loge(String message){
+        Log.e(TAG,message);
+    }
+    public static void loge(String message,Exception exception){
+        Log.e(TAG,message,exception);
+    }
+    public static void loge(Exception exception){
+        Log.e(TAG,"Exception:",exception);
+    }
+}
