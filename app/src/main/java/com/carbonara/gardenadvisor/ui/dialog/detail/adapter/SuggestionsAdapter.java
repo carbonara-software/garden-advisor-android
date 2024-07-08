20303d8d6c2d31740b2286f024e9c988c5f8a6c9
Alex Innocenzi <alex.innocenzi.work@gmail.com>
2024-07-08 15:40:36 +0200

    Added bottomsheet to display suggestions on every plant (#32)
    Ready to be reused on other screens

---
+++ app/src/main/java/com/carbonara/gardenadvisor/ui/dialog/detail/adapter/SuggestionsAdapter.java
@@ -0,0 +1,35 @@
+package com.carbonara.gardenadvisor.ui.dialog.detail.adapter;
+
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView.Adapter;
+import com.carbonara.gardenadvisor.R;
+import com.carbonara.gardenadvisor.ui.dialog.detail.adapter.viewholder.SuggestionViewHolder;
+import java.util.List;
+
+public class SuggestionsAdapter extends Adapter<SuggestionViewHolder> {
+
+  private final List<String> suggestions;
+
+  public SuggestionsAdapter(List<String> suggestions) {
+    this.suggestions = suggestions;
+  }
+
+  @NonNull
+  @Override
+  public SuggestionViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    return new SuggestionViewHolder(
+        LayoutInflater.from(parent.getContext()).inflate(R.layout.row_suggestion, parent, false));
+  }
+
+  @Override
+  public void onBindViewHolder(@NonNull SuggestionViewHolder holder, int position) {
+    holder.setSuggestion(suggestions.get(position));
+  }
+
+  @Override
+  public int getItemCount() {
+    return suggestions.size();
+  }
+}
--- app/src/main/java/com/carbonara/gardenadvisor/ui/home/adapter/viewholder/GardeningItemViewHolder.java
+++ app/src/main/java/com/carbonara/gardenadvisor/ui/home/adapter/viewholder/GardeningItemViewHolder.java
@@ -2,9 +2,11 @@ package com.carbonara.gardenadvisor.ui.home.adapter.viewholder;
 
 import android.view.View;
 import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentActivity;
 import androidx.recyclerview.widget.RecyclerView;
 import com.carbonara.gardenadvisor.ai.dto.GardeningItem;
 import com.carbonara.gardenadvisor.databinding.RowGardeningItemBinding;
+import com.carbonara.gardenadvisor.ui.dialog.detail.SuggestionsBottomSheet;
 
 public class GardeningItemViewHolder extends RecyclerView.ViewHolder {
 
@@ -20,5 +22,17 @@ public class GardeningItemViewHolder extends RecyclerView.ViewHolder {
     this.item = item;
     binding.name.setText(item.getName());
     binding.recommended.setVisibility(item.isRecommended() ? View.VISIBLE : View.GONE);
+    binding
+        .getRoot()
+        .setOnClickListener(
+            v -> {
+              if (binding.getRoot().getContext() != null
+                  && binding.getRoot().getContext() instanceof FragmentActivity) {
+                SuggestionsBottomSheet bottomSheet = new SuggestionsBottomSheet(item);
+                bottomSheet.show(
+                    ((FragmentActivity) binding.getRoot().getContext()).getSupportFragmentManager(),
+                    "DetailBottomSheet");
+              }
+            });
   }
 }
