7b050eecbef25d34b5686b743fd78e0e20a0b800
AlexInn <alex.innocenzi.work@gmail.com>
2024-07-23 16:01:05 +0200

    Fixing Add new Garden Bottom sheet

--- app/src/main/java/com/carbonara/gardenadvisor/ui/garden/GardenFragment.java
+++ app/src/main/java/com/carbonara/gardenadvisor/ui/garden/GardenFragment.java
@@ -8,9 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.carbonara.gardenadvisor.R;
 import com.carbonara.gardenadvisor.databinding.FragmentGardenBinding;
-import com.carbonara.gardenadvisor.persistence.entity.Garden;
 import com.carbonara.gardenadvisor.persistence.entity.GardenWithPlants;
 
 
