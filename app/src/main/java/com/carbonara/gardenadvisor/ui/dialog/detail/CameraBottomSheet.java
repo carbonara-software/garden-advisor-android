9ba69daed6c0aa4718f30cd3323b1b729a320560
Vincenzo Garambone <garambo@gmail.com>
2024-07-25 16:46:56 +0200

    present gemini results

--- app/src/main/java/com/carbonara/gardenadvisor/ui/dialog/detail/CameraBottomSheet.java
+++ app/src/main/java/com/carbonara/gardenadvisor/ui/dialog/detail/CameraBottomSheet.java
@@ -10,9 +10,11 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import com.carbonara.gardenadvisor.ai.GeminiCameraSuggestionWrapper;
 import com.carbonara.gardenadvisor.ai.dto.GeminiCameraSuggestion;
 import com.carbonara.gardenadvisor.databinding.BottomsheetCameraBinding;
+import com.carbonara.gardenadvisor.ui.dialog.detail.adapter.SuggestionsAdapter;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 import com.otaliastudios.cameraview.CameraListener;
 import com.otaliastudios.cameraview.CameraView;
@@ -65,14 +67,14 @@ public class CameraBottomSheet extends BottomSheetDialogFragment {
     takePictureButton.setOnClickListener(v -> camera.takePicture());
 
     binding.close.setOnClickListener(v -> dismiss());
-    binding.errorRetryButton.setOnClickListener(
-        v -> {
-          hideAllLayouts();
-          binding.cameraLayout.setVisibility(View.VISIBLE);
-        });
+    binding.errorRetryButton.setOnClickListener(v -> startOver());
+
+    startOver();
   }
 
   private void pictureTaken(Bitmap bitmap) {
+    binding.pictureView.setImageBitmap(bitmap);
+
     cameraSuggestionWrapper.getGeminiResult(
         bitmap, this::cameraSuggestionSuccess, this::cameraSuggestionFailure);
 
@@ -85,9 +87,45 @@ public class CameraBottomSheet extends BottomSheetDialogFragment {
         .runOnUiThread(
             () -> {
               loge(cameraSuggestion.toString());
-              hideAllLayouts();
-              binding.resultLayout.setVisibility(View.VISIBLE);
+              updatePlantResult(cameraSuggestion);
             });
+  }
+
+  private void updatePlantResult(GeminiCameraSuggestion cameraSuggestion) {
+    binding.plantName.setText(cameraSuggestion.getName());
+    binding.plantScientificName.setText(cameraSuggestion.getScientificName());
+
+    String statusEmoji = "\uD83E\uDD14";
+    String statusDescription = "None";
+
+    switch (cameraSuggestion.getStatus()) {
+      case "healthy":
+        statusEmoji = "\uD83E\uDD29";
+        statusDescription = "Looking good!";
+        break;
+      case "needs_care":
+        statusEmoji = "\uD83E\uDD12";
+        statusDescription = "Might need a little care.\nTake a look at these suggestions:";
+    }
+
+    binding.statusEmoji.setText(statusEmoji);
+    binding.statusText.setText(statusDescription);
+
+    if (cameraSuggestion.getSuggestions() != null && !cameraSuggestion.getSuggestions().isEmpty()) {
+      binding.rvSuggestions.setVisibility(View.VISIBLE);
+      SuggestionsAdapter adapterSuggestions =
+          new SuggestionsAdapter(cameraSuggestion.getSuggestions());
+      LinearLayoutManager llm = new LinearLayoutManager(requireContext());
+      binding.rvSuggestions.setAdapter(adapterSuggestions);
+      binding.rvSuggestions.setLayoutManager(llm);
+    } else {
+      binding.rvSuggestions.setVisibility(View.GONE);
+    }
+
+    binding.successRetryButton.setOnClickListener(v -> startOver());
+
+    hideAllLayouts();
+    binding.resultLayout.setVisibility(View.VISIBLE);
   }
 
   public void cameraSuggestionFailure(Throwable t) {
@@ -98,6 +136,11 @@ public class CameraBottomSheet extends BottomSheetDialogFragment {
               hideAllLayouts();
               binding.errorLayout.setVisibility(View.VISIBLE);
             });
+  }
+
+  private void startOver() {
+    hideAllLayouts();
+    binding.cameraLayout.setVisibility(View.VISIBLE);
   }
 
   private void hideAllLayouts() {
